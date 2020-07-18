9c2162092b63b123aab0312dae33c3d37d69e9d1
timperry0126 <tperry0126@gmail.com>
2020-07-17 21:56:47 -0700

    g

---
+++ test.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+class AnimalTestCase(TestCase):
+
+    def test_animals_can_speak(self):    
+        self.assertEqual(1, 1)
+    def asdf(self):    
+        self.assertEqual(1, 1)
+    def sdfg(self):    
+        self.assertEqual(1, 1)
+    def dfgh(self):    
+        self.assertEqual(1, 1)
+        
